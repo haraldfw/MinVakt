52129b6acb6b45da2ec862b90969d6b5242a3cf0
Gard Steinsvik <gardsteinsvik@gmail.com>
2017-01-11 15:26:39 +0100

    Added layout fragments to be used i layout templates

--- src/main/java/no/ntnu/team5/minvakt/controllers/TestController.java
+++ src/main/java/no/ntnu/team5/minvakt/controllers/TestController.java
@@ -3,10 +3,7 @@ package no.ntnu.team5.minvakt.controllers;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Harald Floor Wilhelmsen on 09.01.2017.
@@ -34,8 +31,8 @@ public class HelloController {
 }*/
 
 @RestController
-public class HelloController {
-    @RequestMapping("/hello")
+public class TestController {
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public Model hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         model.addAttribute("name", name);
         return model;
