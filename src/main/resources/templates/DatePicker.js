e43bdfcaa2d9fb30324591518b5a5d3bb0877801
ingunnsund <ingunnsu@online.no>
2017-01-12 14:50:15 +0100

    Modals and fixes
    Modals and fixes

--- src/main/resources/templates/DatePicker.js
+++ src/main/resources/templates/DatePicker.js
@@ -7,18 +7,18 @@ $(document).ready(function() {
             format: 'dd/mm/yyyy',
             startDate: '01/01/2010',
             endDate: '30/12/2020'
-        })
-        .on('changeDate', function(e) {
-            //fiks tilbakemelding?
-            // Revalidate the date field
-            $('#inputFra, #inputTil').formValidation('revalidateField', 'date');
         });
+    /*.on('changeDate', function(e) {
+     //fiks tilbakemelding?
+     // Revalidate the date field
+     $('#inputFra, #inputTil').formValidation('revalidateField', 'date');*/
+
 
     $("#chooseTime /*#chooseTimeText*/").click(function() {
         if($(this).is(':checked')) {
-            $("#selectTime").css("display", "inline");
+            $("#selectTime, #selectTime2").css("display", "inline");
         } else {
-            $("#selectTime").css("display", "none");
+            $("#selectTime, #selectTime2").css("display", "none");
         }
     });
 
@@ -42,7 +42,41 @@ $(document).ready(function() {
     });
 
 
-    $("")
+    $("#go").click(function() {
+        if(!($("#oneDay").is(":checked"))) {
+            var fra = $("#fraText").val();
+            var nr = fra.split("/");
+            var month = (nr[1]-1);
+            var startDato = new Date(nr[2], month, nr[0]);
+            alert(startDato.toString());
+
+            var til = $("#tilText").val();
+            var nr2 = til.split("/");
+            var month2 = (nr2[1]-1);
+            alert(nr2[0] + " , " + month2 + " , " + nr2[2])
+            var sluttDato = new Date(nr2[2], month2, nr2[0]);
+
+            if(fra > til) {
+                alert("sluttdato kan ikke være før startdato")
+                //TODO virker ikke med år????
+            } else {
+                if($("#chooseTime").is(":checked")) {
+                    //$('.form-group').find('.dropdown-menu').find('li').last().click();$("#selectHour li.selected").text();
+                    var fraTime =$(".btn-group").find(".dropdown-menu").find("li").last().click();
+                    //TODO: alert= [Object object]
+                    alert(fraTime.toString());
+
+                }
+                $("#myModal").modal("toggle");
+            }
+        } else {
+            alert("test");
+            $("#myModal").modal("toggle");
+        }
+
+
+
+    });
 
 });
 /*$("#chooseTimeText").click(function() {
