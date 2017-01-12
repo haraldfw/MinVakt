ee49e08284ebdbacd9fabcd05ba5c2bd496987cf
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2017-01-12 12:45:11 +0100

    username_generation. Early stages

--- src/main/java/no/ntnu/team5/minvakt/dataaccess/UserAccess.java
+++ src/main/java/no/ntnu/team5/minvakt/dataaccess/UserAccess.java
@@ -5,6 +5,10 @@ import org.hibernate.Query;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by alan on 11/01/2017.
  */
@@ -14,7 +18,7 @@ import org.springframework.stereotype.Component;
 public class UserAccess extends Access<User> {
     public User fromID(int userId) {
         return db.transaction(session -> {
-            Query query =  session.createQuery("from User where id = :id");
+            Query query = session.createQuery("from User where id = :id");
             query.setParameter("id", userId);
             return (User) query.uniqueResult();
         });
@@ -26,5 +30,61 @@ public class UserAccess extends Access<User> {
             query.setParameter("username", username);
             return (User) query.uniqueResult();
         });
+    }
+
+    public List<User> getUsernames() {
+        return db.transaction(session -> {
+            Query query = session.createQuery("select user_id, username from User");
+            return (List<User>) query.list();
+        });
+    }
+
+    private String generateUsername(String firstName, String lastName) {
+        final int minLength = 5;
+        final int maxLength = 12;
+
+        int methodsTried = 0;
+        List<User> users = getUsernames();
+        if (users.isEmpty()) {
+            return generateOneUsername(firstName, lastName, methodsTried);
+        }
+        String username = users.get(0).getUsername();
+        while (usernameExists(username, users)) {
+            username = generateOneUsername(firstName, lastName, methodsTried);
+            methodsTried++;
+        }
+        return username;
+    }
+
+    private String generateOneUsername(String firstName, String lastName, int attempts) {
+        firstName = sanitize(firstName.toLowerCase());
+        lastName = sanitize(lastName.toLowerCase());
+
+        // TODO increase fn_chars and ln_chars alternating
+        return expandingSearch(firstName, lastName, 1 + attempts, 1 + attempts);
+    }
+
+    private String expandingSearch(String firstName, String lastName, int firstName_chars, int lastName_chars) {
+        List<String> names = new ArrayList<>();
+
+        String reg = "[ -]";
+        names.addAll(Arrays.asList(firstName.split(reg)));
+        names.addAll(Arrays.asList(lastName.split(reg)));
+        return "";
+    }
+
+    private String sanitize(String s) {
+        s = s.replace("æ", "a").replace("ø", "o").replace("å", "a");
+        s = s.replaceAll("'", "");
+        return s;
+    }
+
+    private boolean usernameExists(String username, List<User> users) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
