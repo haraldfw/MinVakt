db7d9de61be02f8c420213f2fc897a2cd57ee4d1
Knut Kirkhorn <knutkirk@hotmail.com>
2017-01-12 09:51:44 +0100

    New schedule for user
    Made new schedule timesheet for single user.

---
+++ src/main/resources/templates/ScheduleScript.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Knut on 12.01.2017.
+ */
+
+$(document).ready(function() {
+    $(".dayInnhold").click(function() {
+
+    });
+
+    $(".shift").click(function() {
+        //alert("heisann");
+        $("#modalTest").modal("show");
+    });
+});
