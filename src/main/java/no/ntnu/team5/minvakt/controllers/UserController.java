4619eb475c8f5803ca83a284d61e8e0492b3db29
Kenan Mahic <kenanmahic@gmail.com>
2017-01-13 11:16:16 +0100

    Added endpoints for shiftoverview and for a specific users shifts for the next 7 days

--- src/main/java/no/ntnu/team5/minvakt/controllers/UserController.java
+++ src/main/java/no/ntnu/team5/minvakt/controllers/UserController.java
@@ -1,6 +1,8 @@
 package no.ntnu.team5.minvakt.controllers;
 
+import no.ntnu.team5.minvakt.dataaccess.ShiftAccess;
 import no.ntnu.team5.minvakt.dataaccess.UserAccess;
+import no.ntnu.team5.minvakt.db.Shift;
 import no.ntnu.team5.minvakt.db.User;
 import no.ntnu.team5.minvakt.model.LoginResponse;
 import no.ntnu.team5.minvakt.model.NewUser;
@@ -9,6 +11,8 @@ import no.ntnu.team5.minvakt.security.jwt.JWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by alan on 11/01/2017.
@@ -20,6 +24,9 @@ public class UserController {
 
     @Autowired
     private UserAccess userAccess;
+
+    @Autowired
+    private ShiftAccess shiftAccess;
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ResponseEntity<LoginResponse> create(@RequestBody NewUser info) {
@@ -55,5 +62,14 @@ public class UserController {
         JWT.isUser(token, username);
 
         return ResponseEntity.ok().body(userAccess.fromUsername(username));
+    }
+    @RequestMapping(value = "/{username}/nextshifts")
+    public List<Shift> getNextShift (
+            @CookieValue("access_token") String token,
+            @PathVariable("username") String username) {
+
+        JWT.isUser(token, username);
+
+        return shiftAccess.getShiftsForAUser(username);
     }
 }
