0827c1bd4ec6cdba5ad4f3441ed850b964288bd4
ingunnsund <ingunnsu@online.no>
2017-01-13 15:16:14 +0100

    Fixes, added "allshift" code
    Fixes, added "allshift" code

--- src/main/resources/templates/JS/AllShifts.js
+++ src/main/resources/templates/JS/AllShifts.js
@@ -1,3 +1,26 @@
 /**
  * Created by Ingunn on 13.01.2017.
  */
+
+$(document).ready(function() {
+    $("#testing, #testingg").click(function() {
+        if($("#test").css("display") == "none") {
+            $("#test").css("display", "inline");
+        } else {
+            $("#test").css("display", "none");
+        }
+
+
+    });
+    $("#testing2, #testingg2").click(function() {
+        if($("#test2").css("display") == "none") {
+            $("#test2").css("display", "inline");
+        } else {
+            $("#test2").css("display", "none");
+        }
+
+
+
+    });
+
+});
