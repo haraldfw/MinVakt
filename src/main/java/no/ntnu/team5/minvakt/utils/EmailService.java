41a161790a6da2b6b54ef7968af9f4df65c28495
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-01-15 13:00:27 +0100

    Async email-sending

--- src/main/java/no/ntnu/team5/minvakt/utils/EmailService.java
+++ src/main/java/no/ntnu/team5/minvakt/utils/EmailService.java
@@ -1,29 +1,40 @@
 package no.ntnu.team5.minvakt.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Harald on 14.01.2017.
  */
-@Component
-@Scope("singleton")
-public class EmailSender {
+@Service
+public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
 
+    private final Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+    @Async
     public void sendEmail(String to, String subject, String text) {
         sendEmail(to, subject, text, new SimpleMailMessage());
     }
 
+    @Async
     public void sendEmail(String to, String subject, String text, SimpleMailMessage original) {
         original.setTo(to);
         original.setSubject(subject);
         original.setText(text);
-        mailSender.send(original);
+        try {
+            mailSender.send(original);
+        } catch (MailException e) {
+            logger.warn("Error sending email to recipient '" + to + "' with subject '" + subject + "'");
+        }
+
     }
 }
