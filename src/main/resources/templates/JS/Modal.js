d8ffea48a28959bfddd1e8c8db219bc5317fe159
ingunnsund <ingunnsu@online.no>
2017-01-16 12:49:36 +0100

    Forbedringer på fravær
    Forbedringer på fravær

--- src/main/resources/templates/JS/Modal.js
+++ src/main/resources/templates/JS/Modal.js
@@ -42,41 +42,78 @@ $(document).ready(function() {
     });
 
 
+
+    //Legg til fravær
     $("#go").click(function() {
-        if(!($("#oneDay").is(":checked"))) {
-            var fra = $("#fraText").val();
-            var nr = fra.split("/");
-            var month = (nr[1]-1);
-            var startDato = new Date(nr[2], month, nr[0]);
-            alert(startDato.toString());
+        if(!($("#absenceCause").html() === "&nbsp;Fraværsgrunn&nbsp;")) {
+            var fraTid = $("#timeFra").html().split("&nbsp;")[0] + $("#minFra").html().split("&nbsp;")[0];
+            var tilTid = $("#timeTil").html().split("&nbsp;")[0] + $("#minTil").html().split("&nbsp;")[0];
 
-            var til = $("#tilText").val();
-            var nr2 = til.split("/");
-            var month2 = (nr2[1]-1);
-            alert(nr2[0] + " , " + month2 + " , " + nr2[2])
-            var sluttDato = new Date(nr2[2], month2, nr2[0]);
+            if (!($("#oneDay").is(":checked"))) {
+                if ($("#fraText").val() === "" || $("#tilText").val() === "") {
+                    alert("Velg dato");
+                } else {
 
-            if(startDato > sluttDato) {
-                alert("sluttdato kan ikke være før startdato")
-            } else {
-                if($("#chooseTime").is(":checked")) {
-                    var fraTime = $("#timeFra").html().split("&nbsp;")[0];
-                    var fraMin;
-                    var tilTime;
-                    var tilMin;
+                    var fra = $("#fraText").val();
+                    var nr = fra.split("/");
+                    var month = (nr[1] - 1);
+                    var startDato = new Date(nr[2], month, nr[0]);
 
-                    //TODO: alert= [Object object]
-                    alert("startTid: " + fraTime);
+                    var til = $("#tilText").val();
+                    var nr2 = til.split("/");
+                    var month2 = (nr2[1] - 1);
+                    var sluttDato = new Date(nr2[2], month2, nr2[0]);
 
+                    if (startDato > sluttDato) {
+                        alert("sluttdato kan ikke være før startdato")
+                    } else {
+                        if ($("#chooseTime").is(":checked")) {
+                            if ($("#timeFra").html() === "&nbsp;Time&nbsp;" || $("#timeTil").html() === "&nbsp;Time&nbsp;" || $("#minFra").html() === "&nbsp;Min&nbsp;" || $("#minTil").html() === "&nbsp;Min&nbsp;") {
+                                alert("Velg tid checkbox er checked, husk å velg tid");
+                            } else {
+                                if (startDato.getTime() === sluttDato.getTime()) {
+                                    if (fraTid > tilTid) {
+                                        alert("Du kan ikke velge fra tid etter til tid");
+                                    } else {
+                                        $("#absenceModal").modal("hide");
+                                    }
+
+                                } else {
+                                    $("#absenceModal").modal("hide");
+
+                                }
+                            }
+                        } else {
+                            $("#absenceModal").modal("hide");
+                        }
+                    }
                 }
-                $("#myModal").modal("hide");
+            } else {
+                if ($("#fraText").val() === "") {
+                    alert("Velg dato");
+                } else {
+
+                    if ($("#chooseTime").is(":checked")) {
+                        if ($("#timeFra").html() === "&nbsp;Time&nbsp;" || $("#timeTil").html() === "&nbsp;Time&nbsp;" || $("#minFra").html() === "&nbsp;Min&nbsp;" || $("#minTil").html() === "&nbsp;Min&nbsp;") {
+                            alert("Velg tid checkbox er checked, husk å velg tid");
+                        } else {
+
+                            if (fraTid > tilTid) {
+                                alert("Du kan ikke velge fra tid etter til tid");
+                            } else {
+                                $("#absenceModal").modal("hide");
+                            }
+                        }
+                    } else {
+                        $("#absenceModal").modal("hide");
+                    }
+                }
             }
         } else {
-            alert("test");
-            $("#myModal").modal("hide");
+            alert("legg til fraværsgrunn");
         }
-
-
+        //TODO: legg til bedre alert
+        //TODO: legg til tootip
 
     });
 
