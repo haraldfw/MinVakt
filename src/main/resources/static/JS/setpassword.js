890bc1d12e25c59acca9d56172a6925beafd184e
nubk <nicole.ubk@outlook.com>
2017-01-20 12:54:35 +0100

    js ...

--- src/main/resources/static/JS/setpassword.js
+++ src/main/resources/static/JS/setpassword.js
@@ -2,24 +2,29 @@
  * Created by Nicole on 20.01.2017.
  */
 
-$("#rpt-pwd, #new-pwd").on("input", function() { // input = when a key is pressed
-    var newPwd = $("#new-pwd").val();
-    var rptPwd = $("#rpt-pwd").val();
-    $("#check-mark").hide();
-    if(newPwd.length > 0 && newPwd === rptPwd){
-        $("#check-mark").fadeIn(200);
-    }
+$(document).ready(() => {
+
+    $("#rpt-pwd, #new-pwd").on("input", function () { // input = when a key is pressed
+        var newPwd = $("#new-pwd").val();
+        var rptPwd = $("#rpt-pwd").val();
+        $("#check-mark").hide();
+        if (newPwd.length > 0 && newPwd === rptPwd) {
+            $("#check-mark").fadeIn(200);
+        }
+    });
+
+    $("#save-pwd").on("click", function (event) {
+        var newPwd = $("#new-pwd").val();
+        var rptPwd = $("#rpt-pwd").val();
+        if (newPwd !== rptPwd) {
+            event.preventDefault();
+            $("#incorrect-pwd").slideDown(200);
+        }
+    });
+
+    $("#rpt-pwd, #new-pwd").on("focus", function () {
+        $("#incorrect-pwd").slideUp(200);
+    });
+
 });
 
-$("#save-pwd").on("click", function(event) {
-    var newPwd = $("#new-pwd").val();
-    var rptPwd = $("#rpt-pwd").val();
-    if(newPwd !== rptPwd) {
-        event.preventDefault();
-        $("#incorrect-pwd").slideDown(200);
-    }
-});
-
-$("#rpt-pwd, #new-pwd").on("focus", function() {
-    $("#incorrect-pwd").slideUp(200);
-});
