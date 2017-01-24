d76baf7fa29abdf1d429fe4f143cf52200715e3e
ingunnsund <ingunnsu@online.no>
2017-01-24 20:55:07 +0100

    Calendar: add days etc
    Calendar: add days etc

--- src/main/resources/static/JS/Calendar.js
+++ src/main/resources/static/JS/Calendar.js
@@ -13,6 +13,25 @@ $(document).ready(function() {
         $(this).siblings(".cell").css("background-color", "transparent").css("cursor", "auto");
     });
 
+    function dayOfYear(now) { //var now = new Date();
+        var start = new Date(now.getFullYear(), 0, 0);
+        var diff = now - start;
+        var oneDay = 1000 * 60 * 60 * 24;
+        return Math.floor(diff / oneDay); //TODO: test om virker
+    }
+
+    var thisDay = new Date();
+    var weeks = ["week1", "week2", "week3", "week4", "week5", "week6"];
+    //dagnr: td:nth-child(2)
+
+    for(var i = 0; i < weeks.length; i++) {
+
+        for(var j = 2; j < 9; j++) {//8 children, ikke nr 1
+
+
+        }
+
+    }
 
 
 });
