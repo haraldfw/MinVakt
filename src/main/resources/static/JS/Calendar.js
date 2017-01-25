6d338d6975d15613de56384c98a2e2df1b25b521
ingunnsund <ingunnsu@online.no>
2017-01-25 10:14:33 +0100

    Change month works
    Change month works

--- src/main/resources/static/JS/Calendar.js
+++ src/main/resources/static/JS/Calendar.js
@@ -30,12 +30,13 @@ $(document).ready(function() {
         return new Date(date.getTime() + days*24*60*60*1000); //24*60*60*60*1000 is milliseconds in a day
     }
 
+    var month = thisDay.getMonth();
+    var year = thisDay.getFullYear();
+    var firstDay = new Date(year, month, 1);
+
     function plotDays(startDay) {
 
-        var month = thisDay.getMonth();
-        var year = thisDay.getFullYear();
         $("#monthYear").html(months[month] + " " + year);
-        var firstDay = new Date(year, month, 1);
         var firstCalendarDay = addDays(firstDay, -(backDays[firstDay.getDay()]));
 
 
@@ -48,8 +49,7 @@ $(document).ready(function() {
                 if(firstCalendarDay.getMonth() != startDay.getMonth()) {
                     $("#" + weeks[i] + " td:nth-child(" + j + ")").addClass("inactive-month");
                 }
-
-
+                
                 firstCalendarDay = addDays(firstCalendarDay, 1);
             }
         }
@@ -58,13 +58,15 @@ $(document).ready(function() {
     plotDays(thisDay)
 
     function newMonth(way) {
-
-
+        $(".cell").removeClass("today inactive-month");
+        firstDay.setMonth(firstDay.getMonth() + way);
+        month = firstDay.getMonth();
+        year = firstDay.getFullYear();
+        plotDays(firstDay);
     }
 
-
     $(".left").click(function() {
-        newMonth(-1); //fixme fix fix
+        newMonth(-1);
     });
     $(".right").click(function() {
         newMonth(1);
