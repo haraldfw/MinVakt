a47670d8c4029a74fb4ab43d1fa58a6cb271d768
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2017-01-28 15:28:13 +0100

    Javadoc

--- src/main/java/no/ntnu/team5/minvakt/security/PasswordUtil.java
+++ src/main/java/no/ntnu/team5/minvakt/security/PasswordUtil.java
@@ -13,28 +13,56 @@ import java.security.SecureRandom;
  */
 public class PasswordUtil {
     // 512 is the number of bits, SALT_SIZE specifies the number of bytes.
-    private static final int SALT_SIZE = 512/8;
+    private static final int SALT_SIZE = 512 / 8;
 
     private static final SecureRandom secureRandom = new SecureRandom();
 
     private PasswordUtil() {
     }
 
+    /**
+     * Generates a random string in base64
+     *
+     * @return The b64-encoded string
+     */
     public static String generateSalt() {
         byte[] salt = new byte[SALT_SIZE];
         secureRandom.nextBytes(salt);
         return Base64.encodeBase64String(salt);
     }
 
+    /**
+     * Generates a sha512 hash from the given password and salt
+     *
+     * @param password Password to hash
+     * @param salt     Salt to add to password
+     * @return The hashed password
+     */
     public static String generatePasswordHash(String password, String salt) {
         return DigestUtils.sha512Hex(salt + password);
     }
 
+    /**
+     * Ensures that the given password-hash matches the given password and salt
+     *
+     * @param userInput Password entered by user
+     * @param hash      Password-hash from database
+     * @param salt      Salt from database
+     * @return
+     */
     public static boolean verifyPassword(String userInput, String hash, String salt) {
         String inputHashed = generatePasswordHash(userInput, salt);
         return inputHashed.equals(hash);
     }
 
+    /**
+     * Generates a response-object from the given credentials
+     *
+     * @param user     User to check if password matches with
+     * @param password Password to check match with
+     * @param remember If the user ticked the "Remember me" box
+     * @return The response object
+     */
     public static LoginResponse login(User user, String password, boolean remember) {
         boolean isVerified = user != null && PasswordUtil.verifyPassword(password, user.getPasswordHash(), user.getSalt());
 
@@ -50,6 +78,13 @@ public class PasswordUtil {
         return lr;
     }
 
+    /**
+     * Sets the password-hash for the given user with the given password.
+     *
+     * @param user     User to set password for
+     * @param password Password to set for the user
+     * @return True if password is 8 or more chars.
+     */
     public static boolean setPassword(User user, String password) {
         if (password.length() < 8) return false;
 
@@ -60,15 +95,5 @@ public class PasswordUtil {
         user.setPasswordHash(newHash);
 
         return true;
-    }
-
-//    public static void main(String[] args) {
-//        dummyPassword("det");
-//        dummyPassword("det1");
-//        dummyPassword("det2");
-//    }
-
-    private static void dummyPassword(String salt) {
-        System.out.println(generatePasswordHash("passord", salt));
     }
 }
