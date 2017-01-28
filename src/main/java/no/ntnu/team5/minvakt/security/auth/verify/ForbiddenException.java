d0620e4f412ce72537afa67914a1e3fb380812e5
Erlend Tobiassen <erlentob@stud.ntnu.no>
2017-01-28 16:32:01 +0100

    JavaDoc for parts of auth package

--- src/main/java/no/ntnu/team5/minvakt/security/auth/verify/ForbiddenException.java
+++ src/main/java/no/ntnu/team5/minvakt/security/auth/verify/ForbiddenException.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Created by alan on 12/01/2017.
  */
 
+/**
+ * A exception that when cast inside a {@see org.springframework.web.servlet.mvc.Controller} method sets the HttpStatus to {@see org.springframework.http.HttpStatus#UNAUTHORIZED}
+ */
 @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
 public class ForbiddenException extends RuntimeException {
 }
